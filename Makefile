e396b5f9d296f9257e18663ac7e5301f331b32f4
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-06-14 16:33:03 +0100

    Add helloworld example and build flow.
    - Automatically link BSP static library to example app.
    - Need to write a custom linker file to align with the board memory
      space properly. Can this be pinched from the Xilinx SDK?
    
     On branch master
     Changes to be committed:
    	modified:   Makefile
    	new file:   examples/Makefile
    	new file:   examples/helloworld/Makefile
    	new file:   examples/helloworld/hellworld.c
    	modified:   libsrc/Makefile
    	modified:   libsrc/zrb_common.h

--- Makefile
+++ Makefile
@@ -6,8 +6,19 @@ endif
 export CC = $(RISCV)/bin/riscv64-unknown-elf-gcc
 export LD = $(RISCV)/bin/riscv64-unknown-elf-ld
 export AR = $(RISCV)/bin/riscv64-unknown-elf-ar
+export OBJDUMP = $(RISCV)/bin/riscv64-unknown-elf-objdump
 
-all: library
+export CFLAGS += -march=rv32im -mabi=ilp32
 
+export BUILD_DIR = $(shell pwd)/build
+export BSP_LIB   = $(BUILD_DIR)/libzrbbsp.a
+
+all: library examples
+
+.PHONY: library
 library:
 	$(MAKE) -C ./libsrc
+
+.PHONY: examples
+examples: library
+	$(MAKE) -C ./examples
---
+++ examples/helloworld/hellworld.c
@@ -0,0 +1,82 @@
+
+/*!
+@file
+@brief A very simple hello world example for the BSP
+@details Implements a simple program which reads a string of upto 20
+characters from the mailbox, and returns "Hello X!" where X is that string.
+*/
+
+
+#include "zrb_pkg.h"
+
+#define RECV_BUF_LEN 20
+
+void _start() {
+    riscy_main();
+}
+
+/*!
+@brief Entry point for the program
+*/
+int riscy_main () {
+    
+    // Buffer for recieving data from the mailbox FIFO.
+    char recv_buf[RECV_BUF_LEN];
+
+    char resp_buf [] = "Hello ";
+
+    uint32_t recieved;
+    
+    // Handle to the mailbox FIFO
+    zrb_mailbox_t mailbox = ZRB_RISCY_MAILBOX_BASE;
+
+    // Loop forever...
+    while(1) {
+
+        // Recieve some string from the mailbox.
+        for(int i = 0; i < RECV_BUF_LEN; i ++) {
+            
+            // Read from the mailbox using the blocking read function.
+            zrb_mailbox_read(mailbox, &recieved);
+            
+            // Mask the recieved word into a single char.
+            recv_buf [i] = (char)(recieved & 0xFF);
+
+            if(recv_buf[i] == '\0') {
+                // End of the recieved string.
+                continue;
+            }
+
+        }
+        
+
+        // Say hello.
+        for(int i = 0; i < sizeof(resp_buf); i ++) {
+
+            if(resp_buf[i] == '\0') continue;
+            
+            while(zrb_mailbox_send_buffer_full(mailbox)) {
+                // Wait...
+            }
+
+            uint32_t to_send = ((uint32_t)resp_buf[i] & 0xFF);
+            
+            zrb_mailbox_write(mailbox, to_send);
+        }
+
+        // Make our greeting personal...
+        for(int i = 0; i < RECV_BUF_LEN; i ++) {
+            
+            if(recv_buf[i] == '\0') continue;
+            
+            while(zrb_mailbox_send_buffer_full(mailbox)) {
+                // Wait...
+            }
+
+            uint32_t to_send = ((uint32_t)recv_buf[i] & 0xFF);
+            
+            zrb_mailbox_write(mailbox, to_send);
+
+        }
+    }
+}
